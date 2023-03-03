fd92de136176dd496a220609b598a7776c2b34c4
David Maltby <david.maltby@hotmail.com>
2023-03-02 19:47:39 -0600

    Initial implementation

---
+++ MyTestLibrary/Class1.cs
@@ -0,0 +1,13 @@
+﻿[assembly: ArmDot.Client.VirtualizeCode(Enable = false)]
+[assembly: ArmDot.Client.ObfuscateControlFlow(Enable = true)]
+[assembly: ArmDot.Client.ObfuscateNames(Enable = true)]
+[assembly: ArmDot.Client.ObfuscateNamespaces(Enable = true)]
+
+namespace MyTestLibrary;
+
+public class Class1
+{
+    public double SecretSauce() => SecretSauceInternal();
+
+    private double SecretSauceInternal() => Math.Sqrt(64) + 777;
+}
